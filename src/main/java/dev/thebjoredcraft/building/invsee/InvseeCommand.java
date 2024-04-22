059efc8f752fe96ee951a6205af83158b6d34e9a
Red <red.dev@hexle.at>
2024-04-22 19:06:39 +0200

    22.04.2024 19:06 - InvseeCommand.java

--- src/main/java/dev/thebjoredcraft/building/invsee/InvseeCommand.java
+++ src/main/java/dev/thebjoredcraft/building/invsee/InvseeCommand.java
@@ -12,14 +12,33 @@ package dev.thebjoredcraft.building.invsee;
  * penalties, and will be prosecuted to the maximum extent possible under the law.
  */
 
+import dev.thebjoredcraft.building.message.MessageUtil;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class InvseeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(sender instanceof Player player){
+            if(args.length == 1) {
+                Player target = Bukkit.getPlayer(args[0]);
+
+                if(target != null){
+                    player.openInventory(target.getInventory());
+                }else{
+                    player.sendMessage(MiniMessage.miniMessage().deserialize(MessageUtil.PREFIX + "Der Spieler wurde nicht gefunden!"));
+                }
+            }else{
+                player.sendMessage(MiniMessage.miniMessage().deserialize(MessageUtil.PREFIX + "Usage: /invsee <player>"));
+            }
+        }else{
+            sender.sendMessage(MiniMessage.miniMessage().deserialize(MessageUtil.PREFIX + "Du musst ein Spieler sein um diesen Command auszuführen!"));
+        }
         return false;
     }
 }
